f70fe2f10b5574e9ffae517eb63be5d6360f0a92
peipus <peipus@ubuntu.ubuntu-domain>
2012-04-21 19:11:58 +0400

    port

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :via, "scp"
 
 
 
-server "imhotep.ee", :app, :web, :db, :primary => true
+server "-p 22122 imhotepe@imhotep.ee", :app, :web, :db, :primary => true
 set :deploy_to, "/home/imhotepe/rails_app/imhotep"
 set :user, "imhotepe"
 
