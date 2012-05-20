c4420cd7a339f7ea1d5d23f62a68e0134e30c34b
peipus <peipus@ubuntu.ubuntu-domain>
2012-05-20 17:22:23 +0400

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "imhotep"
 set :user, "imhotepe"
 set :domain, "imhotepe@imhotep.ee"
-set :password, '293sLdUt8t'
+set :sudo_password, '293sLdUt8t'
 set :ssh_flags, ['-p 22122']
 set :deploy_to, "/home/imhotepe/rails_apps/imhotep"
 set :repository, 'git@github.com:peipus/imhotep.git'
