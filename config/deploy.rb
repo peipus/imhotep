93cd048d73ac5da1fdc420228b5921635f6f04b4
peipus <peipus@ubuntu.ubuntu-domain>
2012-05-20 17:42:51 +0400

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,14 @@
 set :application, "imhotep"
 set :user, "imhotepe"
 set :domain, "imhotepe@imhotep.ee"
-set :sudo_flags, sudo_flags << '-S'
-set :run_as_sudo, [sudo_cmd, sudo_flags].flatten.compact.join(' ')
-
+set :sudo_cmd
+set :use_sudo, false
 set :sudo_password, '293sLdUt8t'
 set :ssh_flags, ['-p 22122']
 set :deploy_to, "/home/imhotepe/rails_apps/imhotep"
 set :repository, 'git@github.com:peipus/imhotep.git'
+
+ default_run_options[:pty] = true
 
 
 
