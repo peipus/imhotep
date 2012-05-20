43ceaf91ea330b08cd5f9012186b4904face6b9c
peipus <peipus@ubuntu.ubuntu-domain>
2012-05-20 17:01:46 +0400

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "imhotewp"
-set :domain, "imhotep.ee:22122"
+set :domain, "imhotepe@imhotep.ee:22122"
 set :port, "22122"
 set :deploy_to, "/home/imhotepe/rails_apps/imhotep"
 set :repository, 'git@github.com:peipus/imhotep.git'
