fd8f357e0f1cac0a0b5a8d6f92843c4498ff8576
peipus <peipus@ubuntu.ubuntu-domain>
2012-05-20 16:59:46 +0400

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,9 @@
-require 'bundler/capistrano'
-
-default_run_options[:pty] = true
-
-
-
-
-
-set :application, "imhotep"
-set :user, "imhotepe"
-set :repository,  "git@github.com:peipus/imhotep.git"
-
-set :deploy_via, :copy
-set :copy_exclude, [".svn", ".git"]
-
-# set :checkout, 'export'
-
-set :use_sudo, false
-
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-set :port, 22122
-set :password, "293sLdUt8t"
-
+set :application, "imhotewp"
+set :domain, "imhotep.ee:22122"
 set :deploy_to, "/home/imhotepe/rails_apps/imhotep"
+set :repository, 'git@github.com:peipus/imhotep.git'
 
-role :web, "imhotep.ee"                          # Your HTTP server, Apache/etc
-role :app, "imhotep.ee"                          # This may be the same as your `Web` server
-role :db,  "imhotep.ee", :primary => true # This is where Rails migrations will run
-
-set :chmod755, "app config db lib public vendor script script/* public/disp*"
+set :sudo_password, '293sLdUt8t'
 
 
 
@@ -38,17 +12,3 @@ set :chmod755, "app config db lib public vendor script script/* public/disp*"
 
 
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
