cc9b5feba671ef05d7e0bc3a2ba22e238d7bfd62
peipus <peipus@ubuntu.ubuntu-domain>
2012-05-24 18:53:58 +0400

    dep

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Imhotep::Application.initialize!
 
-ENV['GEM_PATH'] = "#{ENV['HOME']}/home/imhotepe/ruby/gems:#{ENV['GEM_PATH']}"
-Gem.clear_paths
+$:.push("/home/imhotepe/ruby/gems")
+
 
 
