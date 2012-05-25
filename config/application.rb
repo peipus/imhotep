e449846c33ad90e5d9b37c5079625bd9e5f56653
peipus <peipus@ubuntu.ubuntu-domain>
2012-05-25 10:32:20 +0400

    dep

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
- $:.push("/home/your_project_name/ruby/gems")
+ $:.push("/home/imhotep/ruby/gems")
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
